30fd05fe8c3fc14292628161fe5d69ff06e9a981
marcelogoodtrip <marcelogoodtrip@gmail.com>
2022-08-31 16:39:02 -0300

    questão 4 resolvida.

--- questions/quest.four.ts
+++ questions/quest.four.ts
@@ -4,3 +4,22 @@
 //  mostrando essas informações conforme exemplo (use a palavra "in" para dentro do intervalo, 
 //  e "out" para fora do intervalo).
 
+function calcInterval() {
+    let vec: any = [5, 14, 123, 10, -25, 32];
+    let min = 10;
+    let max = 20;
+    let inValue:any = [];
+    let outValue:any = [];
+
+    for(let i = 0; i < vec.length; i++) {
+        if(vec[i] >= min && vec[i] <= max) {
+            inValue.push(i);
+        } else if(vec[i] < min && vec[i] > 0 || vec[i] > max ) {
+            outValue.push(i);
+        }
+    }
+
+    return `${inValue.length} in\n${outValue.length} out`
+}
+
+console.log(calcInterval());
