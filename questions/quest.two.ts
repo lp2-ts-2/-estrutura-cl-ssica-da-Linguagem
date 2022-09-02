905080e08837c9531089d5c528c53afc1f4b5cd4
donleon213 <102390549+donleon213@users.noreply.github.com>
2022-09-02 15:08:51 -0300

    Update quest.two.ts

--- questions/quest.two.ts
+++ questions/quest.two.ts
@@ -1,1 +1,10 @@
 //  Ler um número inteiro N e calcular todos os seus divisores.
+
+let x : number = prompt("digite um valor");
+let i : number;
+for(i=x;i>=1;i--){
+    
+    if(x % i == 0){
+        console.log(i)
+    }
+}
